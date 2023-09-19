91dd26822b5d062135a89c1d931998934fac093e
Francisco Batalha <franciscoalexandrefernandesofc@gmail.com>
2023-09-19 10:06:35 -0300

    Update Genero.java

--- cadastroautores/src/main/java/com/example/cadastroautores/model/Genero.java
+++ cadastroautores/src/main/java/com/example/cadastroautores/model/Genero.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.cadastroautores.model;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
 
-/**
- *
- * @author francisco_batalha
- */
+@Entity
+@Getter
+@Setter
 public class Genero {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
 }
