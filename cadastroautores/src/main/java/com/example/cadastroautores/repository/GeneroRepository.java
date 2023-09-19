35af9e841abdb36fc57c982ca9da249121d0fc90
Francisco Batalha <franciscoalexandrefernandesofc@gmail.com>
2023-09-19 10:08:48 -0300

    Update GeneroRepository.java

--- cadastroautores/src/main/java/com/example/cadastroautores/repository/GeneroRepository.java
+++ cadastroautores/src/main/java/com/example/cadastroautores/repository/GeneroRepository.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.example.cadastroautores.repository;
-
-/**
- *
- * @author francisco_batalha
- */
-public interface GeneroRepository {
-    
+import com.example.cadastroautores.model.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface GeneroRepository extends JpaRepository<Genero, Long> {
 }
