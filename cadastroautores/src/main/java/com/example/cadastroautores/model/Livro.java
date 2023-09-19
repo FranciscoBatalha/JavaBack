30d5204dc63638a7c5694789a6d05ac1f3dde4a9
Francisco Batalha <franciscoalexandrefernandesofc@gmail.com>
2023-09-19 10:06:50 -0300

    Update Livro.java

--- cadastroautores/src/main/java/com/example/cadastroautores/model/Livro.java
+++ cadastroautores/src/main/java/com/example/cadastroautores/model/Livro.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.cadastroautores.model;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
 
-/**
- *
- * @author francisco_batalha
- */
+@Entity
+@Getter
+@Setter
 public class Livro {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String pais;
+
 }
+
