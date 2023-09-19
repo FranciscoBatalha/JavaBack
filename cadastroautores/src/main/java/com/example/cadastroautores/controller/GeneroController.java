186af7c8d83218797cede9b08cff0e5ce9d0c26c
Francisco Batalha <franciscoalexandrefernandesofc@gmail.com>
2023-09-19 10:07:47 -0300

    Update GeneroController.java

--- cadastroautores/src/main/java/com/example/cadastroautores/controller/GeneroController.java
+++ cadastroautores/src/main/java/com/example/cadastroautores/controller/GeneroController.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.cadastroautores.controller;
+import com.example.cadastroautores.model.Genero;
+import com.example.cadastroautores.repository.GeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+@CrossOrigin(origins = "http://localhost:5173") // Endereço do front
+@RestController
 
-/**
- *
- * @author francisco_batalha
- */
+@RequestMapping("/generos")
 public class GeneroController {
-    
+ @Autowired
+    private GeneroRepository generoRepository;
+ @GetMapping
+    public List<Genero> listarGeneros() {
+        return generoRepository.findAll();
+    }
+   @PostMapping
+    public Genero criarCategoria(@RequestBody Genero genero) {
+        return generoRepository.save(genero);
+    }
+
 }
+
